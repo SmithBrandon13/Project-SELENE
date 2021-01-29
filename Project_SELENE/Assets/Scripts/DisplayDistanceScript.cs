b1721e9a2ed3c4ebea6824b00ae5893b26024234
B <lilb22128@gmail.com>
2021-01-28 21:05:22 -0600

    Objective and distance display
    -created objective marker
    -created objective mini map marker
    -added world space text display for distance from user to marker
    -added indicator that points to marker
    -added speech controls for toggling map and indicator

---
+++ Project_SELENE/Assets/Scripts/DisplayDistanceScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayDistanceScript : MonoBehaviour
+{
+    private Transform userPos;
+
+    private float distance;
+
+    public TextMeshPro distText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        userPos = GameObject.Find("Main Camera").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(userPos.position, transform.position);
+        int dist = Mathf.RoundToInt(distance);
+        distText.text = dist.ToString() + "m";
+        //print(dist);
+    }
+}
