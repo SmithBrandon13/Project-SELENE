a1c2162d8706ec35958487bcfe7fea3230658250
B <lilb22128@gmail.com>
2021-02-17 20:22:40 -0600

    UI Drafts Addition
    Combined the two scenes and made prefabs out of the UI panels

---
+++ Project_SELENE/Assets/Scripts/MarkerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Persistence;
+
+public class MarkerController : MonoBehaviour
+{
+    public GameObject markerPrefab;
+
+    public Transform user;
+
+    WorldAnchorStore store;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WorldAnchorStore.GetAsync(StoreLoaded);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            SetMarker();
+        }
+    }
+
+    public void SetMarker()
+    {
+        var newMarker = Instantiate(markerPrefab, user.transform.position, Quaternion.identity);
+        newMarker.transform.parent = GameObject.Find("MarkerController").transform;
+    }
+
+    private void StoreLoaded(WorldAnchorStore store)
+    {
+        this.store = store;
+    }
+}
