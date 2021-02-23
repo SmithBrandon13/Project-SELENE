223fd6862945cc7f4bb457866e4a7e5b51b9680d
B <lilb22128@gmail.com>
2021-02-23 12:03:14 -0600

    Waypoint Push
    The waypoint system has been implemented and pulls a prefab hologram that is saved to real world space using the MRTK's world anchors

--- Project_SELENE/Assets/Scripts/MarkerController.cs
+++ Project_SELENE/Assets/Scripts/MarkerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.WSA.Persistence;
+using UnityEngine.XR.WSA;
 
 public class MarkerController : MonoBehaviour
 {
@@ -9,7 +10,11 @@ public class MarkerController : MonoBehaviour
 
     public Transform user;
 
+    public Transform worldSpaceGO;
+
     WorldAnchorStore store;
+
+    WorldAnchor anchor;
 
     // Start is called before the first frame update
     void Start()
@@ -29,11 +34,42 @@ public class MarkerController : MonoBehaviour
     public void SetMarker()
     {
         var newMarker = Instantiate(markerPrefab, user.transform.position, Quaternion.identity);
-        newMarker.transform.parent = GameObject.Find("MarkerController").transform;
+        newMarker.transform.parent = worldSpaceGO;
+        newMarker.name = newMarker.name + transform.position.ToString();
+
+        var newMarkerWA = newMarker.GetComponent<WorldAnchor>();
+
+        SaveGame(newMarker.name, newMarkerWA);
+    }
+
+    public void SetMarkerFromLoad(WorldAnchor thisAcnor)
+    {
+
     }
 
     private void StoreLoaded(WorldAnchorStore store)
     {
         this.store = store;
+        
+        LoadDataFromWorldStore();
+        //if this is ready, then we need to load all the data that we had before
+    }
+
+    private void SaveGame(string id, WorldAnchor thisWorldAnchor)
+    {
+        store.Save(id, thisWorldAnchor);
+    }
+
+    public void LoadDataFromWorldStore()
+    {
+        var currentStores = store.GetAllIds();
+
+        for(int i = 0; i < currentStores.Length; i++)
+        {
+            var loadedMarker = store.Load(currentStores[i], worldSpaceGO.gameObject).gameObject;
+            Instantiate(markerPrefab, loadedMarker.transform.position, Quaternion.identity);
+        }
+
+        
     }
 }
